067babd996d82661edd20cd0d119fcb90c6b0d10
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-12-10 21:39:44 +0100

    add konva

--- js/widget.jsx
+++ js/widget.jsx
@@ -1,15 +1,76 @@
 import * as React from "react";
 import { createRender, useModelState } from "@anywidget/react";
+import { createRoot } from "react-dom/client";
+import { Stage, Layer, Star, Text } from "react-konva";
 import "./widget.css";
 
+function generateShapes() {
+  return [...Array(30)].map((_, i) => ({
+    id: i.toString(),
+    x: Math.random() * 500,
+    y: Math.random() * 400,
+    rotation: Math.random() * 180,
+    isDragging: false,
+  }));
+}
+
+const INITIAL_STATE = generateShapes();
+
 export const render = createRender(() => {
-	const [value, setValue] = useModelState("value");
-	return (
-		<button
-			className="ipykonva-counter-button"
-			onClick={() => setValue(value + 1)}
-		>
-			count is {value}
-		</button>
-	);
+  const [value, setValue] = useModelState("value");
+  const [stars, setStars] = React.useState(INITIAL_STATE);
+
+  const handleDragStart = (e) => {
+    const id = e.target.id();
+    setStars(
+      stars.map((star) => {
+        return {
+          ...star,
+          isDragging: star.id === id,
+        };
+      })
+    );
+  };
+  const handleDragEnd = (e) => {
+    setStars(
+      stars.map((star) => {
+        return {
+          ...star,
+          isDragging: false,
+        };
+      })
+    );
+  };
+
+  return (
+    <Stage width={500} height={400}>
+      <Layer>
+        <Text text="Try to drag a star" />
+        {stars.map((star) => (
+          <Star
+            key={star.id}
+            id={star.id}
+            x={star.x}
+            y={star.y}
+            numPoints={5}
+            innerRadius={20}
+            outerRadius={40}
+            fill="#FFD700"
+            opacity={0.8}
+            draggable
+            rotation={star.rotation}
+            shadowColor="black"
+            shadowBlur={10}
+            shadowOpacity={0.6}
+            shadowOffsetX={star.isDragging ? 10 : 5}
+            shadowOffsetY={star.isDragging ? 10 : 5}
+            scaleX={star.isDragging ? 1.2 : 1}
+            scaleY={star.isDragging ? 1.2 : 1}
+            onDragStart={handleDragStart}
+            onDragEnd={handleDragEnd}
+          />
+        ))}
+      </Layer>
+    </Stage>
+  );
 });
